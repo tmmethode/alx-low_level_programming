6d59885c154128ecaa8acf205265b8ec3e58c468
Methode Twizeyimana <tmmethode1@gmail.com>
2023-07-29 01:58:25 +0200

    new

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function with 3 parameter
+ * @separator: char type pointer to string
+ * @n: unsigned int type
+ *
+ * Return: na
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+	char *ptr;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		ptr = va_arg(ap, char *);
+		if (ptr != NULL)
+			printf("%s", ptr);
+		else
+			printf("(nil)");
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+README FOR 0x10-variadic_functions
