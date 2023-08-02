3ded0b7e5857f0da283fc44627ea577609030615
Methode Twizeyimana <tmmethode1@gmail.com>
2023-08-02 22:08:43 +0200

    new 2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main
- * */
+ * main - Prints all arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	int i, j;
