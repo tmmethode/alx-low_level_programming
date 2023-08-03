3c4a9df5f270e03f0bffd9191e1aa042051d5392
Methode Twizeyimana <tmmethode1@gmail.com>
2023-08-03 21:34:01 +0200

    new

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianess
+ *
+ * Return: 0 if big endian, 1 else
+ */
+int get_endianness(void)
+{
+	int x;
+	char *y;
+
+	x = 1;
+	y = (char *)&x;
+	return (*y);
+}
